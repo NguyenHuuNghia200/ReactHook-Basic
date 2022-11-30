cf677ba825243ea54bbd8b99704098279eba2bbb
nguyen huu nghia <3120218123@ued.udn.vn>
2022-11-30 09:53:43 +0700

    multiple components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/Nav';
 
-function App() {
+const App = () => {
+  let name = "nghia1"
+  let number = 2021
+  let obj = { name: 'eric', number: 2002 }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <Nav />
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello world {name}-{number}
+          </p>
+        </header>
+      </div>
+    </>
   );
 }
 
